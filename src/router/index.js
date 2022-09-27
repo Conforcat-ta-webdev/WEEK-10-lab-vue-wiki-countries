797cd9f6d1e39f2768fb5e3959993c5e0c525b0a
Vicente <vmedina@omitsis.com>
2022-09-27 21:36:36 +0200

    :sparkles: added routes, pages and more

--- src/router/index.js
+++ src/router/index.js
@@ -4,16 +4,19 @@ const routes = [
   {
     path: "/",
     name: "root",
-    component: () =>
-      import(/* webpackChunkName: 'list' */ "../CountriesList.vue"),
+    component: () => import("../pages/IndexView.vue"),
   },
   {
-    path: "/details", // THEN, YOU WILL HAVE TO CHANGE THIS FOR A PARAM!
-    name: "details",
-    component: () =>
-      import(
-        /* webpackChunkName: 'details' */ "../components/CountryDetails.vue"
-      ),
+    path: "/list",
+    name: "list",
+    component: () => import("../pages/CountriesListView.vue"),
+    children: [
+      {
+        path: "/details", // THEN, YOU WILL HAVE TO CHANGE THIS FOR A PARAM!
+        name: "details",
+        component: () => import("../pages/CountryDetails.vue"),
+      },
+    ],
   },
 ];
 
