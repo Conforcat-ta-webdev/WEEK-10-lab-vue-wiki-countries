da31d550861c2339570d7473f134454e78774601
Vicente <vmedina@omitsis.com>
2022-09-29 19:42:03 +0200

    :sparkles: added country borders

---
+++ src/components/CountryBorders.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <li v-for="border in borders" :key="border">
+      <a :href="getLinkToDetail(border)">{{ getCountryName(border) }}</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+import countriesJson from "../countries.json";
+export default {
+  props: {
+    borders: Array,
+  },
+  methods: {
+    getCountryName(alpha3Code) {
+      return [...countriesJson].filter((c) => c.alpha3Code === alpha3Code)[0]
+        ?.name.common;
+    },
+    getLinkToDetail(alpha3Code) {
+      return "/" + alpha3Code;
+    },
+  },
+};
+</script>
+<style scope>
+li {
+  list-style-type: none;
+}
+</style>
