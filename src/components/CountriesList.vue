a3d76b68cde35d5f27f22272e109549404ecc3d9
Arturo Rubio <arubio@seocom.agency>
2022-09-30 10:25:40 +0200

    done

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -1,10 +1,36 @@
 <script setup>
-import countries from "../countries.json"
+import { ref, reactive, onMounted } from "vue"
+
+// ?? xk al poner 'countries' como 'ref' sí funcionaba y con 'reactive' no?
+// -> con 'reactive' hemos de definir una key y la respuesta del fetch asociarla a esta key
+//     ya que sino al hacer 'countries = json' sobreescribía la variable reactiva y se perdía la reactividad
+const countries = reactive({list: []})
+// const countries = ref(null) --> con 'ref' tamb funciona
+const loading = ref(true);
+const error = ref(null)
+
+// ?? mejor definir las variables con 'const' o con 'let'?
+// -> las variables 'ref' o 'reactivas' mejor con const xk, por ejemplo, 
+//     luego te pueden sobreescribr la variable y pierda la reactividad, como me ha pasado
+
+onMounted(async () => {
+    try {
+        const res = await fetch("https://ih-countries-api.herokuapp.com/countries")
+        if (res.ok) countries.list = await res.json()
+        if (!res.ok) error.value = "Error! Could not reach the API."
+        loading.value = !loading.value
+    } catch (error) {
+        error.value = "Error! Could not reach the API."
+        loading.value = !loading.value
+    }
+})
 </script>
 
 <template>
+    <p v-if="error">{{ error }}</p>
+    <p v-if="loading">Loading info...</p>
     <RouterLink
-        v-for="country in countries"
+        v-for="country in countries.list"
         :key="country._id"
         class="list-group-item list-group-item-action"
         :to="country.alpha3Code"
--- src/components/CountryDetails.vue
+++ src/components/CountryDetails.vue
@@ -1,42 +1,61 @@
 <script setup>
-import countries from "../countries.json"
 import { useRoute } from 'vue-router'
-import { computed, watch } from 'vue';
+import { ref, computed, onMounted, watch } from 'vue';
 
 const route = useRoute()
-let country = computed(() => countries.filter(country => country.alpha3Code === route.params.id))
+const country = ref(null)
+const loading = ref(true)
+const error = ref(null)
+// ?? xk al poner 'apiUrl' con 'computed' sí funciona y con 'ref' o 'reactive' no?
+// -> xk comuted cambia en base a una variable ref o reactive, que es lo que sería route
+const apiUrl = computed(() => `https://ih-countries-api.herokuapp.com/countries/${route.params.id}`)
 
-watch(route, newValue => {
-    country = countries.filter(country => country.alpha3Code === newValue.params.id)
-})
+function fetchData() {
+    fetch(apiUrl.value)
+        .then(res => {
+            if (!res.ok) error.value = "Error! Could not reach the API."
+            return res.json()
+        })
+        .then(data => country.value = data)
+        .catch(err => error.value = err)
+        .then(() => loading.value = false)
+}
+    
+onMounted(() => fetchData())
+
+watch(route => fetchData())
 </script>
 
 <template>
-    <img :src="`https://flagpedia.net/data/flags/icon/72x54/${country[0].alpha2Code.toLowerCase()}.png`" alt="country flag" style="width: 100px"/>
-    <h1>{{ country[0].name.common }}</h1>
-    <table class="table">
-        <thead></thead>
-        <tbody>
-        <tr>
-            <td style="width: 30%">Capital</td>
-            <td>{{ country[0].capital.join(', ') }}</td>
-        </tr>
-        <tr>
-            <td>Area</td>
-            <td>
-                {{ country[0].area }} km <sup>2</sup>
-            </td>
-        </tr>
-        <tr>
-            <td>Borders</td>
-            <td>
-            <ul>
-                <li v-for="border in country[0].borders" :key="border">
-                    <RouterLink :to="border">{{ border }}</RouterLink>
-                </li>                
-            </ul>  
-            </td>
-        </tr>
-        </tbody>
-    </table>
+    <p v-if="error">{{ error }}</p>
+    <p v-if="loading">Loading info...</p>
+    <div v-if="country">
+        <img :src="`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`" alt="country flag" style="width: 100px"/>
+        <h1>{{ country.name.common }}</h1>
+        <table class="table">
+            <thead></thead>
+            <tbody>
+                <tr>
+                    <td style="width: 30%">Capital</td>
+                    <td>{{ country.capital.join(', ') }}</td>
+                </tr>
+                <tr>
+                    <td>Area</td>
+                    <td>
+                        {{ country.area }} km <sup>2</sup>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Borders</td>
+                    <td>
+                        <ul>
+                            <li v-for="border in country.borders" :key="border">
+                                <RouterLink :to="border">{{ border }}</RouterLink>
+                            </li>                
+                        </ul>  
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </template>
