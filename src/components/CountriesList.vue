a0403b7c5f6ef4e8f049791af2a54374120e3a8e
Alicia Barrios <aliciabgo@gmail.com>
2022-10-01 15:00:54 +0200

    modificando estilos

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -12,7 +12,7 @@
 </script>
 
 <template>
-  <div class="col-5" style="max-height: 90vh; overflow: scroll">
+  <div class="col-5 scroll" style="max-height: 90vh; overflow-x: auto">
     <div class="list-group"> 
       <RouterLink 
         v-for="country in countriesSorted" 
@@ -26,6 +26,33 @@
     </div>
   </div>
 </template>
+
+<style scoped>
+  .scroll::-webkit-scrollbar {
+    appearance: none;
+  }
+
+  .scroll::-webkit-scrollbar:vertical {
+    width:10px;
+  }
+
+  .scroll::-webkit-scrollbar-button:increment,.contenedor::-webkit-scrollbar-button {
+    display: none;
+  } 
+
+  .scroll::-webkit-scrollbar:horizontal {
+    height: 10px;
+  }
+
+  .scroll::-webkit-scrollbar-thumb {
+    background-color: #797979;
+    border-radius: 20px;
+    border: 2px solid #f1f2f3;
+  }
+  .scroll::-webkit-scrollbar-track {
+    border-radius: 10px;  
+  }
+</style>
 
 
 
