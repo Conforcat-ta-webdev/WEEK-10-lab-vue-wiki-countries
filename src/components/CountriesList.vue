0ac15ba7daa4f9f02159e6a7e7d1116c4573cc4f
Boris Michael Randebrock <boris@randebrock.com>
2022-10-03 12:24:22 +0200

    heroku fetch added

--- src/components/CountriesList.vue
+++ src/components/CountriesList.vue
@@ -1,10 +1,29 @@
 <script setup>
-import countries from "../countries.json";
+// import countries from "../countries.json";
+import { ref, reactive, onMounted } from "vue";
+
+const countries = reactive({ list: [] });
+const loading = ref(true);
+const error = ref(null);
+
+onMounted(async () => {
+  try {
+    const res = await fetch("https://ih-countries-api.herokuapp.com/countries");
+    if (res.ok) countries.list = await res.json();
+    if (!res.ok) error.value = "Error - API unavailable";
+    loading.value = !loading.value;
+  } catch (error) {
+    error.value = "Error - API unavailable";
+    loading.value = !loading.value;
+  }
+});
 </script>
 
 <template>
+  <p v-if="error">{{ error }}</p>
+  <p v-if="loading">fetching... :)</p>
   <RouterLink
-    v-for="country in countries"
+    v-for="country in countries.list"
     :key="country._id"
     class="list-group-item list-group-item-action"
     :to="country.alpha3Code"
--- src/components/CountryDetails.vue
+++ src/components/CountryDetails.vue
@@ -1,46 +1,85 @@
 <script setup>
 import countries from "../countries.json";
 import { useRoute } from "vue-router";
-import { computed, watch } from "vue";
+import { computed, watch, ref, onMounted } from "vue";
+
 const route = useRoute();
-let country = computed(() =>
-  countries.filter((country) => country.alpha3Code === route.params.id)
+// let country = computed(() =>
+//   countries.filter((country) => country.alpha3Code === route.params.id)
+// );
+const country = ref(null);
+const loading = ref(true);
+const error = ref(null);
+const apiUrl = computed(
+  () => `https://ih-countries-api.herokuapp.com/countries/${route.params.id}`
 );
-watch(route, (newValue) => {
-  country = countries.filter(
-    (country) => country.alpha3Code === newValue.params.id
-  );
-});
+
+// watch(route, (newValue) => {
+//   country = countries.filter(
+//     (country) => country.alpha3Code === newValue.params.id
+//   );
+// });
+
+function fetchData() {
+  fetch(apiUrl.value)
+    .then((res) => {
+      if (!res.ok) error.value = "Error - API unavailable";
+      return res.json();
+    })
+    .then((data) => (country.value = data))
+    .catch((err) => (error.value = err))
+    .then(() => (loading.value = false));
+}
+
+onMounted(() => fetchData());
+watch((route) => fetchData());
 </script>
 
 <template>
-  <img
+  <p v-if="error">{{ error }}</p>
+  <p v-if="loading">fetching... :)</p>
+  <div v-if="country">
+    <!-- <img
     :src="`https://flagpedia.net/data/flags/icon/72x54/${country[0].alpha2Code.toLowerCase()}.png`"
     alt="country flag"
     style="width: 100px"
-  />
-  <h1>{{ country[0].name.common }}</h1>
-  <table class="table">
-    <thead></thead>
-    <tbody>
-      <tr>
-        <td style="width: 30%">Capital</td>
-        <td>{{ country[0].capital.join(", ") }}</td>
-      </tr>
-      <tr>
-        <td>Area</td>
-        <td>{{ country[0].area }} km <sup>2</sup></td>
-      </tr>
-      <tr>
-        <td>Borders</td>
-        <td>
-          <ul>
-            <li v-for="border in country[0].borders" :key="border">
-              <RouterLink :to="border">{{ border }}</RouterLink>
-            </li>
-          </ul>
-        </td>
-      </tr>
-    </tbody>
-  </table>
+  /> -->
+    <img
+      :src="`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`"
+      alt="country flag"
+      style="width: 100px"
+    />
+    <!-- <h1>{{ country[0].name.common }}</h1> -->
+    <h1>{{ country.name.common }}</h1>
+    <table class="table">
+      <thead></thead>
+      <tbody>
+        <tr>
+          <td style="width: 30%">Capital</td>
+          <!-- <td>{{ country[0].capital.join(", ") }}</td> -->
+          <td>{{ country.capital.join(", ") }}</td>
+        </tr>
+        <tr>
+          <td>Area</td>
+          <!-- <td>{{ country[0].area }} km <sup>2</sup></td> -->
+          {{
+            country.area
+          }}
+          km
+          <sup>2</sup>
+        </tr>
+        <tr>
+          <td>Borders</td>
+          <td>
+            <ul>
+              <!-- <li v-for="border in country[0].borders" :key="border"> -->
+              <li v-for="border in country.borders" :key="border">
+                <RouterLink :to="border">{{ border }}</RouterLink>
+              </li>
+            </ul>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
 </template>
