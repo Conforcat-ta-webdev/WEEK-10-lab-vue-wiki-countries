530e89e0bdee547be96852b5f26f297ff3daf046
Lua Varela <lua_varela12@hotmail.com>
2022-10-05 00:15:47 +0200

    Misión imposible part 9

---
+++ src/components/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+            <a class="navbar-brand" href="/">LAB-WikiCountries</a>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+}
+</script>
